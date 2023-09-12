245cf923387a892237759be2edd7ebeed0586cbb
Yaoming Wen <yaomingwen@microsoft.com>
2023-09-12 15:01:28 +0800

    Finish second version

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
 using System.Globalization;
+using System.Text.Json;
 
 /*
     * 0    TIMESTAMP	
@@ -98,7 +99,7 @@ using System.Globalization;
     * 91   tenant.domain	
     * 92   tenant.id
     */
-public class RawSpan
+internal class RawSpan
 {
     [Index(8)]
     public string ServiceName { get; set; }
@@ -120,7 +121,6 @@ public class RawSpan
     public string ParentId { get; set; }
     [Index(19)]
     public string Success { get; set; }
-    public string Tags { get; set; }
     [Index(12)]
     public string Source { get; set; }
     [Index(25)]
@@ -152,50 +152,83 @@ public class RawSpan
     public RawSpan() { }
 }
 
-public class Span
+internal class Tags
 {
-    public string serviceName { get; set; }
-    public string spanId { get; set; }
-    public string traceId { get; set; }
-    public string startTimeUtc { get; set; }
-    public string duration { get; set; }
-    public string kind { get; set; }
-    public string displayName { get; set; }
-    public string operationName { get; set; }
-    public string parentId { get; set; }
-    public string status { get; set; }
-    public string tags { get; set; }
-    public string events { get; set; }
-    public string links { get; set; }
-    public string source { get; set; }
+    public string HttpHost { get; set; }
+    public string HttpMethod { get; set; }
+    public string HttpStatusCode { get; set; }
+    public string HttpUrl { get; set; }
+    public string HttpRequestHeaderMsCv { get; set; }
+    public string HttpRequestHeaderScenariotag { get; set; }
+    public string HttpResponseHeaderRequestId { get; set; }
+    public string HttpResponseHeaderXBeserver { get; set; }
+    public string HttpResponseHeaderXDiaginfo { get; set; }
+    public string HttpTarget { get; set; }
+    public string HttpUserAgent { get; set; }
+    public string HttpFlavor { get; set; }
+    public string HttpScheme { get; set; }
+    public Tags(RawSpan rawSpan)
+    {
+        HttpHost = rawSpan.HttpHost;
+        HttpMethod = rawSpan.HttpMethod;
+        HttpStatusCode = rawSpan.HttpStatusCode;
+        HttpUrl = rawSpan.HttpUrl;
+        HttpRequestHeaderMsCv = rawSpan.HttpRequestHeaderMsCv;
+        HttpRequestHeaderScenariotag = rawSpan.HttpRequestHeaderScenariotag;
+        HttpResponseHeaderRequestId = rawSpan.HttpResponseHeaderRequestId;
+        HttpResponseHeaderXBeserver = rawSpan.HttpResponseHeaderXBeserver;
+        HttpResponseHeaderXDiaginfo = rawSpan.HttpResponseHeaderXDiaginfo;
+        HttpTarget = rawSpan.HttpTarget;
+        HttpUserAgent = rawSpan.HttpUserAgent;
+        HttpFlavor = rawSpan.HttpFlavor;
+        HttpScheme = rawSpan.HttpScheme;
+    }
+}
+internal class Span
+{
+    public string ServiceName { get; set; }
+    public string SpanId { get; set; }
+    public string TraceId { get; set; }
+    public string StartTimeUtc { get; set; }
+    public string Duration { get; set; }
+    public string Kind { get; set; }
+    public string DisplayName { get; set; }
+    public string OperationName { get; set; }
+    public string ParentId { get; set; }
+    public string Status { get; set; }
+    public string Tags { get; set; }
+    public string Events { get; set; }
+    public string Links { get; set; }
+    public string Source { get; set; }
     public Span(RawSpan rawSpan)
     {
-        serviceName = rawSpan.ServiceName;
-        spanId = rawSpan.SpanId;
-        traceId = rawSpan.TraceId;
-        startTimeUtc = rawSpan.StartTimeUtc;
-        duration = DateTime.Parse(rawSpan.EndTimeUtc).Subtract(DateTime.Parse(rawSpan.StartTimeUtc)).ToString();
-        kind = rawSpan.Kind;
-        displayName = rawSpan.DisplayName;
-        operationName = rawSpan.OperationName;
-        parentId = rawSpan.ParentId;
-        status = rawSpan.Success == "TRUE" ? "success" : "fail";
-        source = rawSpan.Source;
-        events = "";
-        links = "";
-        tags = $"{rawSpan.HttpHost};{rawSpan.HttpMethod};{rawSpan.HttpStatusCode};{rawSpan.HttpUrl};{rawSpan.HttpRequestHeaderMsCv};{rawSpan.HttpRequestHeaderScenariotag};{rawSpan.HttpResponseHeaderRequestId};{rawSpan.HttpResponseHeaderXBeserver};{rawSpan.HttpResponseHeaderXDiaginfo};{rawSpan.HttpTarget};{rawSpan.HttpUserAgent};{rawSpan.HttpFlavor};{rawSpan.HttpScheme}";
+        ServiceName = rawSpan.ServiceName;
+        SpanId = rawSpan.SpanId;
+        TraceId = rawSpan.TraceId;
+        StartTimeUtc = rawSpan.StartTimeUtc;
+        TimeSpan span = DateTime.Parse(rawSpan.EndTimeUtc).Subtract(DateTime.Parse(rawSpan.StartTimeUtc));
+        Duration = Convert.ToInt32(span.TotalMilliseconds * 1000).ToString();
+        Kind = rawSpan.Kind;
+        DisplayName = rawSpan.DisplayName;
+        OperationName = rawSpan.OperationName;
+        ParentId = rawSpan.ParentId;
+        Status = rawSpan.Success == "TRUE" ? "success" : "fail";
+        Source = rawSpan.Source;
+        Events = "";
+        Links = "";
+        Tags = JsonSerializer.Serialize(new Tags(rawSpan));
     }
 
     public override string ToString()
     {
-        return $"\"{serviceName}\",\"{spanId}\",\"{traceId}\",\"{startTimeUtc}\",\"{duration}\",\"{kind}\",\"{displayName}\",\"{operationName}\",\"{parentId}\",\"{status}\",\"{tags}\",\"{events}\",\"{links}\",\"{source}\"";
+        return $"{ServiceName}\t{SpanId}\t{TraceId}\t{StartTimeUtc}\t{Duration}\t{Kind}\t{DisplayName}\t{OperationName}\t{ParentId}\t{Status}\t{Tags}\t{Events}\t{Links}\t{Source}";
     }
 }
 
-class Converter
+internal class Converter
 {
     readonly static private string InputPath = "input.csv";
-    readonly static private string OutputPath = "output.csv";
+    readonly static private string OutputPath = "output.tsv";
     readonly static private HashSet<string> parents = new();
     readonly static private List<Span> allSpans = new();
     readonly static private Stack<Span> filteredSpans = new();
@@ -203,13 +236,12 @@ class Converter
     private static void GenerateOutputCSV()
     {
         using StreamWriter sw = new(OutputPath);
-        sw.WriteLine("serviceName,spanId,traceId,startTimeUtc,duration,kind,displayName,operationName,parentId,status,tags,events,links,source");
+        sw.WriteLine("ServiceName\tSpanId\tTraceId\tStartTimeUtc\tDuration\tKind\tDisplayName\tOperationName\tParentId\tStatus\tTags\tEvents\tLinks\tSource");
         while (filteredSpans.Count > 0)
         {
             sw.WriteLine(filteredSpans.Pop());
         }
     }
-
 
     private static void ParseInputCSV()
     {
@@ -220,29 +252,27 @@ class Converter
             HeaderValidated = null,
             MissingFieldFound = null
         };
-        using (var sw = new StreamReader(InputPath))
-        using (var csv = new CsvReader(sw, config))
+        using var sr = new StreamReader(InputPath);
+        using var csv = new CsvReader(sr, config);
+        var records = csv.GetRecords<RawSpan>();
+        foreach (var record in records)
         {
-            var records = csv.GetRecords<RawSpan>();
-            foreach (var record in records)
-            {
-                Span span = new Span(record);
-                allSpans.Add(span);
-            }
+            Span span = new(record);
+            allSpans.Add(span);
         }
     }
-    
+
     private static void FilterAndDistinct()
     {
         int count = allSpans.Count();
         for (int i = count - 1; i >= 1; i--)
         {
             Span span = allSpans[i];
-            if (span.parentId == "" || parents.Contains(span.parentId))
+            if (span.ParentId == "" || parents.Contains(span.ParentId))
             {
-                if (!parents.Contains(span.spanId))
+                if (!parents.Contains(span.SpanId))
                 {
-                    parents.Add(span.spanId); // distinct
+                    parents.Add(span.SpanId); // distinct
                     filteredSpans.Push(span);
                 }
             }
